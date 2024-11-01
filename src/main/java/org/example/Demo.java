8edb6fce9cfdcf96ab0a0652d3afd078a7ef54ff
Denis43t <javadenisstudy@gmail.com>
2024-11-01 19:42:07 +0100

    finished tasks

--- src/main/java/org/example/Demo.java
+++ src/main/java/org/example/Demo.java
@@ -2,28 +2,36 @@ package org.example;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.thymeleaf.TemplateEngine;
 
 import org.thymeleaf.context.Context;
-import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import java.io.IOException;
-import java.util.LinkedHashMap;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
-@WebServlet("/test")
+
+@WebServlet("/time")
 public class Demo extends HttpServlet {
+    ZonedDateTime date;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     private TemplateEngine engine;
+
 
     @Override
     public void init() throws ServletException {
         engine = new TemplateEngine();
 
-        FileTemplateResolver resolver = new FileTemplateResolver();
-        resolver.setPrefix("./templates/");
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("templates/");
         resolver.setSuffix(".html");
         resolver.setTemplateMode("HTML5");
         resolver.setOrder(engine.getTemplateResolvers().size());
@@ -33,21 +41,50 @@ public class Demo extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int checkOnPresentCookies = 0;
         resp.setContentType("text/html");
 
-        Map<String, String[]> parameterMap = req.getParameterMap();
+        String timezone = req.getParameter("timezone");
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null && timezone == null) {
+            checkOnPresentCookies = 1;
+        }
 
-        Map<String, Object> params = new LinkedHashMap<>();
-        for (Map.Entry<String, String[]> keyValue : parameterMap.entrySet()) {
-            params.put(keyValue.getKey(), keyValue.getValue()[0]);
+        if (checkOnPresentCookies == 0) {
+            if (timezone == null) {
+                date = ZonedDateTime.now();
+                timezone = String.valueOf(date.getOffset());
+            } else if (timezone.contains("-")) {
+                timezone = timezone.replaceAll("UTC", "");
+                resp.addCookie(new Cookie("lastTimezone", timezone));
+                ZoneOffset zoneOffSet = ZoneOffset.of(timezone);
+                date = OffsetDateTime.now(zoneOffSet).toZonedDateTime();
+            } else {
+                timezone = timezone.replaceAll("UTC ", "");
+                timezone = "+".concat(timezone);
+
+                resp.addCookie(new Cookie("lastTimezone", timezone));
+
+                ZoneOffset zoneOffSet = ZoneOffset.of(timezone);
+                date = OffsetDateTime.now(zoneOffSet).toZonedDateTime();
+            }
+        } else {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("lastTimezone")) {
+                    timezone = cookie.getValue();
+                    ZoneOffset zoneOffSet = ZoneOffset.of(timezone);
+                    date = OffsetDateTime.now(zoneOffSet).toZonedDateTime();
+                }
+            }
         }
 
         Context simpleContext = new Context(
                 req.getLocale(),
-                Map.of("queryParams", params)
+                Map.of("queryParams", date.format(formatter) + " UTC" + timezone)
         );
 
         engine.process("test", simpleContext, resp.getWriter());
+        timezone=null;
         resp.getWriter().close();
     }
 }
